4b9d99f7a39d9ec850fd12173d2f6c1166e0b6ca
crayyy_zee <65814383+i-am-zaidali@users.noreply.github.com>
2023-06-15 23:23:32 +0500

    idek man

--- modplus/views.py
+++ modplus/views.py
@@ -385,7 +385,6 @@ class ActionSelectView(ViewDisableOnTimeout):
 
         command = getattr(self.cog, select.values[0]) 
         ctx.command = command
-        ctx.cog = command.cog
         await command(ctx, **kwargs)
 
         await inter.response.send_message("Action completed.")
